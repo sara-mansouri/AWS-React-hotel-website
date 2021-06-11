9d554de3e8c9d77180008f637d3291cf1bb8ff74
Sara Mansouri <s.mansoury@gmail.com>
2021-06-10 17:09:52 -0700

    Dynamo DB table scripts-region update

--- src/scripts/CreateServicesTable.js
+++ src/scripts/CreateServicesTable.js
@@ -1,7 +1,7 @@
 var AWS = require("aws-sdk");
 
 AWS.config.update({
-  region: "us-east-1"
+  region: "us-east-2",
 });
 
 var dynamodb = new AWS.DynamoDB();
@@ -10,20 +10,21 @@ var params = {
   TableName: "Services",
   KeySchema: [
     // Partition Key
-    { AttributeName: "name", KeyType: "HASH" }  
+    { AttributeName: "name", KeyType: "HASH" },
   ],
-  AttributeDefinitions: [
-    { AttributeName: "name", AttributeType: "S" }
-  ], 
+  AttributeDefinitions: [{ AttributeName: "name", AttributeType: "S" }],
   ProvisionedThroughput: {
     ReadCapacityUnits: 10,
-    WriteCapacityUnits: 10
-  }
+    WriteCapacityUnits: 10,
+  },
 };
 
-dynamodb.createTable(params, function(err, data) {
+dynamodb.createTable(params, function (err, data) {
   if (err)
-    console.error("Unable to create table: ", JSON.stringify(err, null, 2))
+    console.error("Unable to create table: ", JSON.stringify(err, null, 2));
   else
-    console.log("Created table with description: ", JSON.stringify(data, null, 2))
+    console.log(
+      "Created table with description: ",
+      JSON.stringify(data, null, 2)
+    );
 });
