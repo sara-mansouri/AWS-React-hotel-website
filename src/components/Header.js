bc906790ce0051c5d0c186da339b65cd72d1bb80
Sara Mansouri <s.mansoury@gmail.com>
2021-06-11 18:39:44 -0700

    fetching data from DynamoDb API

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,81 @@
-import React from 'react';
-import menuLinksData from './data/menu_links.json'
+import React, { useState, useEffect } from "react";
+// import menuLinksData from "./data/menu_links.json";
+// import { useState } from 'react';
 
 const Header = () => {
+  // const [menuLinksData, setmenuLinksData] = useState([]);
+
+  // const loadMenuLinksData = async () => {
+  //   //Query the API Gateway
+  //   const response = await fetch(
+  //     "https://ly9cioaa8j.execute-api.us-east-2.amazonaws.com/Production/menuLinks"
+  //   );
+  //   let jsonData = response.json();
+  //   //assign data to state variable
+
+  //   setmenuLinksData(jsonData);
+  // };
+  // useEffect(() => {
+  //   //load menu link dara
+  //   loadMenuLinksData();
+  // }, []);
+  /**/
+
+  const [menuLinksData, setMenuLinksData] = useState([]);
+
+  const loadMenuLinksData = async () => {
+    // Query the API Gateway
+    const resp = await fetch(
+      "https://ly9cioaa8j.execute-api.us-east-2.amazonaws.com/Production/menuLinks"
+    );
+    let jsonData = await resp.json();
+
+    // Assign response data to our state variable
+    setMenuLinksData(jsonData);
+  };
+
+  useEffect(() => {
+    // Load the menu links data from the API Gateway
+    loadMenuLinksData();
+  }, []);
+
   return (
     <header id="intro">
       <article className="fullheight">
         <div className="hgroup">
           <h1>Landon Hotel</h1>
           <h2>West London</h2>
-          <p><a href="#welcome"><img src="https://landonhotel.com/images/misc/arrow.png" alt="down arrow" /></a></p>
+          <p>
+            <a href="#welcome">
+              <img
+                src="https://landonhotel.com/images/misc/arrow.png"
+                alt="down arrow"
+              />
+            </a>
+          </p>
         </div>
       </article>
 
       <nav id="nav">
         <div className="navbar">
-          <div className="brand"><a href="#welcome">Landon <span>Hotel</span></a></div>
+          <div className="brand">
+            <a href="#welcome">
+              Landon <span>Hotel</span>
+            </a>
+          </div>
           <ul>
-            {
-              menuLinksData.map((link) =>
-                <li><a className={`icon ${link.class}`} href={link.href}><span>{link.text}</span></a></li>
-              )
-            }
+            {menuLinksData.map((link) => (
+              <li>
+                <a className={`icon ${link.class}`} href={link.href}>
+                  <span>{link.text}</span>
+                </a>
+              </li>
+            ))}
           </ul>
         </div>
       </nav>
     </header>
   );
-}
+};
 
 export default Header;
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,21 +1,58 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
 const Welcome = () => {
+  const [gallayImagesData, setgallayImagesData] = useState([]);
+
+  const loadgallayImagesData = async () => {
+    // Query the API Gateway
+    const resp = await fetch(
+      "https://ly9cioaa8j.execute-api.us-east-2.amazonaws.com/Production/galleryImages"
+    );
+    let jsonData = await resp.json();
+
+    // Assign response data to our state variable
+    setgallayImagesData(jsonData);
+  };
+
+  useEffect(() => {
+    // Load the menu links data from the API Gateway
+    loadgallayImagesData();
+  }, []);
+
   return (
     <div className="scene" id="welcome">
       <article className="content">
         <div className="gallery">
-          <img src="https://landonhotel.com/images/hotel/intro_room.jpg" alt="Intro Gallery Room Sample Pictures" />
+          {gallayImagesData.map((galleryImage) => (
+            <img
+              src={galleryImage.src}
+              alt={galleryImage.alt}
+              className={galleryImage.className}
+            />
+          ))}
+
+          {/* <img src="https://landonhotel.com/images/hotel/intro_room.jpg" alt="Intro Gallery Room Sample Pictures" />
           <img src="https://landonhotel.com/images/hotel/intro_pool.jpg" alt="Intro Gallery Pool Sample Pictures" />
           <img src="https://landonhotel.com/images/hotel/intro_dining.jpg" alt="Intro Gallery Dining Sample Pictures" />
           <img src="https://landonhotel.com/images/hotel/intro_attractions.jpg" alt="Intro Gallery Attractions Sample Pictures" />
-          <img className="hidesm" src="https://landonhotel.com/images/hotel/intro_wedding.jpg" alt="Intro Gallery Dining Sample Pictures" />
+          <img className="hidesm" src="https://landonhotel.com/images/hotel/intro_wedding.jpg" alt="Intro Gallery Dining Sample Pictures" /> */}
         </div>
         <h1>Welcome to the Landon&nbsp;Hotel</h1>
-        <p>The original Landon perseveres after 50 years in the heart of West London. The West End neighborhood has something for everyone—from theater to dining to historic sights. And the not-to-miss Rooftop Cafe is a great place for travelers and locals to engage over drinks, food, and good&nbsp;conversation. &nbsp;To learn more about the Landon Hotel in the West End, browse our website and <a href="files/landon_information_sheet_London.pdf">download our handy information sheet</a>.</p>
+        <p>
+          The original Landon perseveres after 50 years in the heart of West
+          London. The West End neighborhood has something for everyone—from
+          theater to dining to historic sights. And the not-to-miss Rooftop Cafe
+          is a great place for travelers and locals to engage over drinks, food,
+          and good&nbsp;conversation. &nbsp;To learn more about the Landon Hotel
+          in the West End, browse our website and{" "}
+          <a href="files/landon_information_sheet_London.pdf">
+            download our handy information sheet
+          </a>
+          .
+        </p>
       </article>
     </div>
   );
-}
+};
 
 export default Welcome;
